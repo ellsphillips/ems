f2a7754e0a89afbd22db3885fefc28e2bf8b71ad
ellsphillips <ellsphillipsuni@gmail.com>
2021-09-13 20:15:43 +0100

    Add employee types

--- EMS/components/employee.py
+++ EMS/components/employee.py
@@ -10,6 +10,7 @@ class Employee(ABC):
     """
     Basic representation of an employee at the company.
     """
+
     name: str
     role: Role
     vacation_days: int = 25
@@ -31,3 +32,31 @@ class Employee(ABC):
             return
         self.vacation_days -= FIXED_VACATION_DAYS_PAYOUT
         print(f"Paying out a holiday. Holidays left: {self.vacation_days}")
+
+
+@dataclass
+class HourlyEmployee(Employee):
+    """
+    Employee that's paid based on number of hours worked.
+    """
+    hourly_rate: float = 50
+    hours_worked: int = 10
+
+    def pay(self) -> None:
+        print(
+            f"Paying employee {self.name} a hourly rate of \
+            ${self.hourly_rate} for {self.hours_worked} hours."
+        )
+
+
+@dataclass
+class SalariedEmployee(Employee):
+    """
+    Employee that's paid based on a fixed monthly salary.
+    """
+    monthly_salary: float = 5000
+
+    def pay(self) -> None:
+        print(
+            f"Paying employee {self.name} a monthly salary of ${self.monthly_salary}."
+        )
