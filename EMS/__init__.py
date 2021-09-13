4efa2925501cd4ca51eb86554c0e52b907fddbfc
ellsphillips <ellsphillipsuni@gmail.com>
2021-09-13 20:39:42 +0100

    Set up module imports

---
+++ EMS/__init__.py
@@ -0,0 +1,3 @@
+from .components.company import Company
+from .components.employee import Employee
+from .components.roles import Role
--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-from EMS.components.roles import Role
-from EMS.components.employee import HourlyEmployee, SalariedEmployee
-from EMS.components.company import Company
+import EMS
 
 
 def main() -> None:
-    company = Company()
+    company = EMS.Company()
 
-    company.add_employee(SalariedEmployee(name="Michael Scott", role=Role.MANAGER))
-    company.add_employee(SalariedEmployee(name="Pam Beasley", role=Role.WORKER))
-    company.add_employee(SalariedEmployee(name="Jim Halpert", role=Role.WORKER))
-    company.add_employee(HourlyEmployee(name="Ryan Howard", role=Role.INTERN))
+    company.add_employee(EMS.SalariedEmployee(name="Michael Scott", role=EMS.Role.MANAGER))
+    company.add_employee(EMS.SalariedEmployee(name="Pam Beasley", role=EMS.Role.WORKER))
+    company.add_employee(EMS.SalariedEmployee(name="Jim Halpert", role=EMS.Role.WORKER))
+    company.add_employee(EMS.HourlyEmployee(name="Ryan Howard", role=EMS.Role.INTERN))
 
-    print(company.find_employees(role=Role.MANAGER))
-    print(company.find_employees(role=Role.WORKER))
-    print(company.find_employees(role=Role.INTERN))
+    print(company.find_employees(role=EMS.Role.MANAGER))
+    print(company.find_employees(role=EMS.Role.WORKER))
+    print(company.find_employees(role=EMS.Role.INTERN))
 
     for employee in company.employees:
         employee.pay()
     
     company.employees[0].take_a_holiday()
+
 
 if __name__ == "__main__":
     main()
