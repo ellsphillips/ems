c05dad71d30a2e643a1b7b089c8ec6f4f2ac9c90
ellsphillips <ellsphillipsuni@gmail.com>
2021-09-13 20:22:01 +0100

    Build company and test method calls

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-def main() -> None:
-    pass
+from EMS.components.roles import Role
+from EMS.components.employee import HourlyEmployee, SalariedEmployee
+from EMS.components.company import Company
 
+
+def main() -> None:
+    company = Company()
+
+    company.add_employee(SalariedEmployee(name="Michael Scott", role=Role.MANAGER))
+    company.add_employee(SalariedEmployee(name="Pam Beasley", role=Role.WORKER))
+    company.add_employee(SalariedEmployee(name="Jim Halpert", role=Role.WORKER))
+    company.add_employee(HourlyEmployee(name="Ryan Howard", role=Role.INTERN))
+
+    print(company.find_employees(role=Role.MANAGER))
+    print(company.find_employees(role=Role.WORKER))
+    print(company.find_employees(role=Role.INTERN))
 
 if __name__ == "__main__":
     main()
